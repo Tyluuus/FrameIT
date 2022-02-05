31bed56b0112be282b66354bd851ed5156f2f0fc
Tyluuus <tylus97@gmail.com>
2022-02-05 12:09:45 +0100

    Initial Commit

---
+++ FrameIT/FrameITApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameITApp.swift
+//  FrameIT
+//
+//  Created by Piotr Tyl on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FrameITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
